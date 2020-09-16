665317af38a21df4c811c3ac4c7ef64cd0381127
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-09-16 12:11:57 -0500

    Add transaction page

--- main.go
+++ main.go
@@ -44,6 +44,7 @@ func main() {
 	router.GET("/", renderHome)
 	router.GET("/:blockchain/:network", renderNetwork)
 	router.GET("/:blockchain/:network/block/:id", renderBlock)
+	router.GET("/:blockchain/:network/block/:id/tx/:hash", renderBlockTransaction)
 	router.GET("/:blockchain/:network/account/:address", renderAccountBalance)
 
 	log.Fatal(router.Run(opts.listenAddr))
@@ -162,6 +163,44 @@ func renderBlock(c *gin.Context) {
 		"network":           netID,
 		"block":             blockResp.Block,
 		"otherTransactions": blockResp.OtherTransactions,
+	})
+}
+
+func renderBlockTransaction(c *gin.Context) {
+	client := getClient(c)
+	netID := getNetworkID(c)
+
+	blockID := &types.BlockIdentifier{}
+	txID := &types.TransactionIdentifier{
+		Hash: c.Param("hash"),
+	}
+
+	// Parse out the block identifier
+	id := c.Param("id")
+	if len(id) > 16 {
+		blockID.Hash = id
+	} else {
+		var index int64
+		fmt.Sscanf(id, "%d", &index)
+		blockID.Index = index
+	}
+
+	txResp, rosettaErr, err := client.BlockAPI.BlockTransaction(
+		context.Background(),
+		&types.BlockTransactionRequest{
+			NetworkIdentifier:     netID,
+			BlockIdentifier:       blockID,
+			TransactionIdentifier: txID,
+		},
+	)
+	if shouldAbort(c, rosettaErr, err) {
+		return
+	}
+
+	c.HTML(200, "transaction.html", gin.H{
+		"network": netID,
+		"block":   blockID,
+		"tx":      txResp.Transaction,
 	})
 }
 
